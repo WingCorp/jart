d14d4df1b7b09e498a47926d76c9564320bf44a7
Kasper Hjort Berthelsen <khjo@itu.dk>
2018-07-03 20:19:22 +0200

    add AmbientLights

--- tracer/src/Light.kt
+++ tracer/src/Light.kt
@@ -1,1 +1,7 @@
-data class Light(val position: Point, val colour: Colour, val intensity: Float)
+interface Light {
+    val colour: Colour
+    fun direction(p: Point): Vector
+    fun shadow( p: Point) : Boolean
+    val geometricFactor: Float
+    val probabilityDensity: Float
+}
--- tracer/src/Scene.kt
+++ tracer/src/Scene.kt
@@ -1,1 +1,1 @@
-data class Scene(val shapes: List<Shape>)
+data class Scene(val shapes: List<Shape>, val lights: List<Light>, val ambientLights: List<AmbientLight>)
